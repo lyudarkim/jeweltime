9594faeef1ea87bead5cc69d78c7733d5a264744
Lyuda Kim <lyudarkim@gmail.com>
2023-07-13 11:45:06 -0700

    Register blueprints

---
+++ app/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+        # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('RENDER_DATABASE_URI')
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
+    # Import models here for Alembic setup
+    from app.models.account import Account
+    from app.models.project import Project
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register Blueprints here
+    from .routes.account import accounts_bp
+    from .routes.project import projects_bp
+
+    app.register_blueprint(accounts_bp)
+    app.register_blueprint(projects_bp)
+
+    return app
