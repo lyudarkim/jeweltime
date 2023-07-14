cfff8eea0809f8059615ffbf0ea5e5002474ed20
Lyuda Kim <lyudarkim@gmail.com>
2023-07-14 09:31:31 -0700

    Fix migration issues, downgrade version of SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ def create_app(test_config=None):
     app.config['CORS_HEADERS'] = 'Content-Type'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "SQLALCHEMY_DATABASE_URI"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
     # Import models here for Alembic setup
     from app.models.account import Account
